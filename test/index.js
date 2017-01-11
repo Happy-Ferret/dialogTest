ca8bf8c5ffb3551738ea4d4e80d9fc7f30eb963b
Happy-Ferret <MarkBauer@gmx.ch>
2017-01-11 11:55:16 +0100

    Update index.js
    - Added action button to spawn further surfaces
    - Added window enumeration.
      - for benchmarking purposes, console.log returns the number of open windows/surfaces every time the action button is pressed and a
        new surface is generated.

--- test/index.js
+++ test/index.js
@@ -1,17 +1,39 @@
 const { Cc, Ci } = require('chrome');
 var { openDialog } = require('sdk/window/utils');
-// var window = openDialog("chrome://browser/content/aboutDialog.xul");
-var window = openDialog({
-    // No "url" supplied here in this case as we add it below (in order to have a ready listener in place before load which can give us access to the tab worker)
-    // For more, see https://developer.mozilla.org/en-US/docs/Web/API/window.open#Position_and_size_features
-    features: Object.keys({
-        chrome: false, // Needed for centerscreen per docs
-        centerscreen: true, // Doesn't seem to be working for some reason (even though it does work when calling via XPCOM)
-        resizable: true,
-        scrollbars: true
-    }).join() + ',width=850,height=650',
-    name: "My window name " + Math.random().toString(36).substring(7),
-    url:"chrome://browser/content/emoji/main.xul"
-    // parent: 
-    // args: 
+var buttons = require('sdk/ui/button/action');
+
+var button = buttons.ActionButton({
+  id: "cmd-link",
+  label: "Open commandline",
+  icon: {
+    "16": "./icon.svg",
+    "32": "./icon.svg",
+    "64": "./icon.svg"
+  },
+  onClick: handleClick
 });
+
+function openSurface() {
+    var window = openDialog({
+        // No "url" supplied here in this case as we add it below (in order to have a ready listener in place before load which can give us access to the tab worker)
+        // For more, see https://developer.mozilla.org/en-US/docs/Web/API/window.open#Position_and_size_features
+        features: Object.keys({
+            chrome: false, // Needed for centerscreen per docs
+            centerscreen: true, // Doesn't seem to be working for some reason (even though it does work when calling via XPCOM)
+            resizable: true,
+            scrollbars: true
+        }).join() + ',width=850,height=650',
+        name: "My window name " + Math.random().toString(36).substring(7),
+        url:"chrome://browser/content/emoji/main.xul"
+        // parent: 
+        // args: 
+    });
+    var windowList = (windows())
+    for (var windowNo in windowList) {
+        console.log(windowNo)
+    }
+}
+
+function handleClick(state) {
+  openSurface();
+}
